346382f32c6226bdd6413f9eacebc9f105373264
Lucas-Duarte-dev <Lucasduarteb2000@hotmail.com>
2022-06-06 21:28:25 -0300

    feat: iniciando a aplicação

---
+++ src/domain/CustomerInterface.ts
@@ -0,0 +1,15 @@
+export interface CustomerInterface {
+    id: string
+
+    first_name: string
+
+    last_name: string
+
+    username: string
+
+    password: string
+
+    created_at: Date
+
+    updated_at: Date
+}
